0d17c4b88124ba9bb041ec76a4f424f856bd4d76
Aneta <anc@praqma.net>
2023-09-07 11:40:51 +0200

    git add changed the category names

--- components/Map/MapGl.tsx
+++ components/Map/MapGl.tsx
@@ -21,31 +21,11 @@ export default function MapGl({ posts }: MapGiProps) {
   const [viewState, setViewState] = useState({
     latitude: 69.65065246406847,
     longitude: 18.956804295081962,
-    zoom: 12,
+    zoom: 10,
   });
-
-  // const [isHovered, setIsHovered] = useState(false);
-
-  // const debouncedHandleMouseEnter = debounce((title) => {
-  //   setIsHovered(true);
-  //   setName(title);
-  // }, 150);
-
-  // const handlOnMouseLeave = () => {
-  //   setIsHovered(false);
-  //   debouncedHandleMouseEnter.cancel();
-  //   setName("");
-  // };
 
   return (
     <div className={`${styles.mapwhole} desktop`}>
-      {/* <div className={styles.textContainer}>
-        <h2 className={styles.mainTitle}>
-          <span className="purple">{pagedata.title}</span>
-          <br />
-          {pagedata.description}
-        </h2>
-      </div> */}
       <Map
         style={{ width: "100vw", height: "500px", position: "relative" }}
         {...viewState}
